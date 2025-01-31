81623661710488c5ab6eeea4ea49940a827c65e4
ValckeMalo <malovalcke@gmail.com>
2025-01-31 10:56:28 +0100

    set up som monster with action and ui

--- Assets/Scripts/Unit/Monsters/Brain/MonsterBrain.cs
+++ Assets/Scripts/Unit/Monsters/Brain/MonsterBrain.cs
@@ -16,19 +16,14 @@ namespace OMG.Unit.Monster.Brain
         [SerializeField, ReadOnly] private UnitAction nextAction;
         public UnitAction NextAction => nextAction;
 
-        public void Action(IUnit player, IUnit monster, IUnit[] monsters)
-        {
-            if (nextAction == null) return;
-
-            nextAction.Execute(player);
-            nextAction = null;
-        }
         public void Action(IUnit player, IUnit monster)
         {
             if (nextAction == null) return;
 
-            Debug.Log($"Action play is {nextAction.name}");
-            nextAction.Execute(player);
+            if (nextAction.name.Split("Blindage").Length > 1)
+                nextAction.Execute(monster);
+            else nextAction.Execute(player);
+
             nextAction = null;
         }
 
